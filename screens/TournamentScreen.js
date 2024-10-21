57bfd406c847a411ffaa746bf7da9837859e70d4
Alexandr <sparkygino@gmail.com>
2024-10-21 22:05:01 +0300

    tournament screen

--- screens/TournamentScreen.js
+++ screens/TournamentScreen.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import Navbar from "../components/Navbar";
 import { getDatabase, ref, set } from "firebase/database";
@@ -45,20 +46,120 @@ export default function SupportScreen({ navigation }) {
             </View>
         </View>
         <Text style={[styles.league__title,{color: themeStyles.textColor}]}>Черная лига</Text>
-        <View style={[styles.hr, {backgroundColor: themeStyles.hr}]}></View>
-        <View style={styles.leaderboard}>
-            <View style={styles.leaderboard__item}>
-
-            </View>
-            <View style={styles.leaderboard__item}></View>
+        <View style={[styles.hr, {backgroundColor: themeStyles.hr}]}>
+       
 
         </View>
+        <ScrollView style={styles.scrollView}>
+        <View style={styles.leaderboard}>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>1</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Артём</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>2</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Александр</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>3</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Михаил</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>4</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Артём</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>5</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Артём</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.derank_container}>
+            <View style={[styles.hr_derank, {backgroundColor: themeStyles.hr}]}></View>
+        <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>Зона понижения</Text>
+        <View style={[styles.hr_derank, {backgroundColor: themeStyles.hr}]}></View>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>6</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Артём</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>7</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Артём</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>8</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Артём</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>9</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Артём</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+            <View style={styles.leaderboard__item}>
+              <View style={styles.leaderboard__place}>
+              <Text style={[styles.text, styles.place, {color: themeStyles.textColor}]}>10</Text>
+              <Image source={require('../assets/user-avatar.png')} style={styles.userScore__img}/>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor}]}>Артём</Text>
+              </View>
+              <Text style={[styles.userScore__text, {color: themeStyles.textColor, right: 10}]}>1000 pt</Text>
+            </View>
+           
+        </View>
+        </ScrollView>
       <Navbar />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+  },
+  leaderboard__place : {
+    display: 'flex', flexDirection: 'row',gap: 30, alignItems: 'center', 
+  },
+  place: {
+    width: 30,
+    fontSize: 22,
+    position: "relative",
+    fontWeight: "bold",
+    left: 5
+  },
+ 
   container: {
     flex: 1,
     justifyContent: "center",
@@ -79,6 +180,12 @@ const styles = StyleSheet.create({
     fontSize: 28,
     position: "relative",
     fontWeight: "bold"
+  },
+  userScore__text: {
+    color: "#FFF",
+    fontSize: 24,
+    position: "relative",
+    fontWeight: "400"
   },
   headerAbout: {
     position: "absolute",
@@ -144,9 +251,32 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 250
   },
+  hr_derank: {
+    width: 104,
+    height: 1,
+    position: 'absolute',
+    top: 250
+  },
   leaderboard__item: {
     width: 381,
     height: 54,
-    backgroundColor: "#F5F5F5"
+    backgroundColor: "#F5F5F5",
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  userScore__img: {
+    width: 40,
+    height: 40
+  },
+  leaderboard: {
+    top: 270,
+    position: 'relative'
+  },
+  derank_container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
   }
 });
